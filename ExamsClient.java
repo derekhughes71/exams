f88f96404be38c729745c857db31b6e0913908e5
derekhughes71 <derekhughes2014@u.northwestern.edu>
2016-04-17 02:59:51 -0400

    Added files via upload

--- ExamsClient.java
+++ ExamsClient.java
@@ -4,26 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
+
 public class ExamsClient {
 		
 		// begin main method
-		 public static void main(String[] args) throws IOException, SQLException{
+		 public static void main(String[] args) throws Exception{
 			 
 //	THIS IS USING ImportTxtFile class to import
 
-		// read in .txt files (rooms.txt, courses.txt, buildings.txt)
+// 		read in .txt files (rooms.txt, courses.txt, buildings.txt)
 //			 ImportTxtFile importRooms = new ImportTxtFile();
 //			 importRooms.readFile("rooms.txt");
-//			 
+			 
 //			 ImportTxtFile importCourses = new ImportTxtFile();
 //			 importCourses.readFile("courses.txt");
+//			 System.out.println(importCourses.allColumns.size());
 			 
 			 ImportTxtFile importBuildings = new ImportTxtFile(); // 26 rows and 2 columns for buildings.txt
-			//importBuildings.getNumRows();	
 			 importBuildings.readFile("buildings.txt"); // insert .txt to read and parse
-			 //importBuildings.createColumn(0); // insert column index value for particular column (based on readFile() txt file)
+			 //System.out.println(importBuildings.allColumns.size());
+			 DbConnect.post(importBuildings.allColumns); // insert columns into table fields in database
+			 
 
-			 importBuildings.insertColumn(); 		 
+
+			 // Test for proper data being retrieved in arraylist
+//			 for( int i = 0; i <= (importBuildings.allColumns.size() - 1); i++) {
+//				   String[] us = importBuildings.allColumns.get( i );
+//				  
+//				   for(int j=0; j < us.length; j++)
+//					{
+//						System.out.println("This is row"+j+" of array #"+i+": "+us[j]);
+//					}				   
+//			 }
+			 
+			 //importBuildings.insertColumn(); 		 
 			 
 			 
 			 // parse .txt files into tables using arrays
